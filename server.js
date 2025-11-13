691127aeb32e711298c96a3c2e2c9e8dc6091ceb
vijay kumar <vijaykumar06232@gmail.com>
2025-11-13 22:34:25 +0530

    project fix

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+app.use((req,res) => {
+  console.log(Date.now());
+})
 // Routes
 app.get("/", (req, res) => {
   res.json({
