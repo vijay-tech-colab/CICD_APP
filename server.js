c6e2d82033312c399fd6290a001ea05e1a7c753d
Vijay Kumar <vijaykumar06232@gmail.com>
2025-11-14 14:45:24 +0530

    added post api

--- server.js
+++ server.js
@@ -57,6 +57,46 @@ app.get("/users", async (req, res) => {
   }
 });
 
+// Create User API
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    // Validation
+    if (!name || !email) {
+      return res.status(400).json({
+        success: false,
+        message: "Name and Email are required",
+      });
+    }
+
+    // Check duplicate email
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.status(409).json({
+        success: false,
+        message: "Email already exists",
+      });
+    }
+
+    // Create new user
+    const newUser = await User.create({ name, email });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error while creating user",
+    });
+  }
+});
+
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 
